3720389b74d73d1378f062a158874d57bb6aff7d
jazbogross <57679104+jazbogross@users.noreply.github.com>
2024-12-10 03:14:12 +0100

    streamable true

--- sender.py
+++ sender.py
@@ -9,7 +9,8 @@ def start_sender(host, port):
         "v4l2src device=/dev/video0 ! videoconvert ! "
         "x264enc speed-preset=ultrafast tune=zerolatency ! h264parse ! queue ! mux. "
         "alsasrc device=hw:1 ! audioconvert ! audioresample ! opusenc ! opusparse ! queue ! mux. "
-        f"matroskamux name=mux ! tcpserversink host=0.0.0.0 port={port}"
+        "matroskamux name=mux streamable=true ! "
+        f"tcpserversink host=0.0.0.0 port={port}"
     )
 
     os.system(sender_pipeline)
