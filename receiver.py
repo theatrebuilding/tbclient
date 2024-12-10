2f99e8042290cad88b56b8c33893dee88d5a9e7b
jazbogross <57679104+jazbogross@users.noreply.github.com>
2024-12-10 12:11:20 +0100

    change receiver host

--- receiver.py
+++ receiver.py
@@ -6,7 +6,7 @@ def start_receiver(host, port):
     
     receiver_pipeline = (
         f"gst-launch-1.0 -v "
-        f"tcpclientsrc host=0.0.0.0 port={port} ! matroskademux name=d "
+        f"tcpclientsrc host={host} port={port} ! matroskademux name=d "
         "d.video_0 ! queue ! h264parse ! avdec_h264 ! videoconvert ! autovideosink "
         "d.audio_0 ! queue ! opusdec ! audioconvert ! autoaudiosink"
     )
