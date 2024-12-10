18a0b02ecfb7b00060fbdf30360b4833feb725df
jazbogross <57679104+jazbogross@users.noreply.github.com>
2024-12-10 15:08:58 +0100

    gstreamer test

--- sender.py
+++ sender.py
@@ -3,14 +3,12 @@ import os
 def start_sender(host, port):
     # Start the sender pipeline
     print("Starting sender...")
+    print("Host: " + host)
+    print("Port: " + port)
+
 
     sender_pipeline = (
-        f"gst-launch-1.0 -v "
-        "v4l2src device=/dev/video0 ! videoconvert ! "
-        "x264enc speed-preset=ultrafast tune=zerolatency ! h264parse ! queue ! mux. "
-        "alsasrc device=hw:1 ! audioconvert ! audioresample ! opusenc ! opusparse ! queue ! mux. "
-        "matroskamux name=mux streamable=true ! "
-        f"tcpserversink host={host} port={port}"
+        f"gst-launch-1.0 tcpclientsrc host={host} port={port} ! fdsink fd = 2"
     )
 
     os.system(sender_pipeline)
