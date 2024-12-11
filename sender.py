7ca38967f6301ea3a98c1313462c35b57d78a820
jazbogross <57679104+jazbogross@users.noreply.github.com>
2024-12-11 19:33:38 +0100

    gstreamer test

--- sender.py
+++ sender.py
@@ -8,9 +8,12 @@ def start_sender(host1, sender):
 
 
     sender_pipeline = (
-        f"gst-launch-1.0 "
-        f"v4l2src device=/dev/video0 ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast "
-        f"! rtph264pay ! queue ! tcpclientsink host={host1} port={sender}"
+        f"gst-launch-1.0 -v "
+        f"videotestsrc ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast "
+        f"! rtph264pay config-interval=1 pt=96 ! tcpclientsink host={host1} port={sender}"
     )
 
     os.system(sender_pipeline)
+
+
+
