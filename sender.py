56b3e05cb9709e5693a39f1c91d4f49c81104301
kevintf <kevinthorfleming@gmail.com>
2024-12-11 19:04:12 +0100

    sender changes

--- sender.py
+++ sender.py
@@ -10,7 +10,7 @@ def start_sender(host1, sender):
     sender_pipeline = (
         f"gst-launch-1.0 "
         f"v4l2src device=/dev/video0 ! videoconvert ! h264enc tune=zerolatency bitrate=500 speed-preset=superfast "
-        f"! rtph264pay ! queue ! tcpclientsink host={host1} port={sender}"
+        f"! rtph264pay ! queue ! tcpserversink host={host1} port={sender}"
     )
 
     os.system(sender_pipeline)
